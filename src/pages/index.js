858dcf6c52cbcc78a1a5ed66528e88d3a5586779
Charouf <charoufnassro@gmail.com>
2021-07-10 00:44:18 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => {
   return (
     <LayoutComponent>
       <Head title="Home "/>
-      <h1>Hello</h1>
+      <h1>Hello CHAROUF</h1>
       <h2>I'm nassro, a full-stack developper living in beautiful algeria</h2> 
       <p>Need a developer ?<Link to="/contact">Contact me!</Link></p>
     </LayoutComponent>
