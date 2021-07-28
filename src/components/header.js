92f78fc3ebfb369d99e932330924686d55954c90
Charouf <charoufnassro@gmail.com>
2021-07-28 14:32:56 +0100

    Update header to check the auto deploy of gatsby cloud

--- src/components/header.js
+++ src/components/header.js
@@ -21,7 +21,7 @@ const HeaderComponent = () => {
         <ul className={headerStyles.navList}>
           <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link></li>
           <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} partiallyActive={true} to="/blog/">Blog</Link></li>
-          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about/">About</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about/">About-update</Link></li>
           <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact/">Contact</Link></li>
         </ul>
       </nav>
